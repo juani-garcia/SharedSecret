f57ca8fa8aca9acbaf25e7f5aca3b5f59c940ac4
shoni <juanigarcia@itba.edu.ar>
2023-06-17 12:51:19 -0300

    Remove prints

--- src/distribution.c
+++ src/distribution.c
@@ -22,8 +22,6 @@ static void generate_polynomials(const uint8_t* block, Polynomial* f, Polynomial
     }
     uint8_t r = uniform(1, mod-1);
 
-    printf("r-value: %d\n", r);
-
     for(int i = 0; i < 2; i++) {
         uint8_t a = f->coeffs[i] == 0 ? 1 : f->coeffs[i];
         g->coeffs[i] = ((mod - 1) * r * a) % mod;
@@ -52,28 +50,13 @@ uint8_t  **generate_shadows(uint8_t* data, size_t data_size, uint8_t k, uint8_t 
 
     for (uint16_t i = 0; i < block_count; i++) {
         uint8_t* curr_block = data + i * (2*k-2);
-        printf("Block %d:\n", i);
         generate_polynomials(curr_block, &f, &g, mod);
-
-        printf("Polynomial f: ");
-        for(int iter = 0; iter < f.degree + 1; iter++){
-            printf("%d*x^%d + ", f.coeffs[iter], iter);
-        }
-        puts("");
-
-        printf("Polynomial g: ");
-        for(int iter = 0; iter < g.degree + 1; iter++){
-            printf("%d*x^%d + ", g.coeffs[iter], iter);
-        }
-        puts("");
 
         for(int j = 0; j < n; j++) {
             uint8_t *sj = shadows[j];
             sj[2*i] = eval(&f, j + 1, mod);
             sj[2*i + 1] = eval(&g, j + 1, mod);
-            printf("v(%d, %d) = %d, %d\n", i+1, j+1, sj[2*i], sj[2*i + 1]);
         }
-        puts("\n-----------------------------------------");
     }
 
     free(f.coeffs);
