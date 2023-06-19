0ffea3e3a139778f161ae203b5df7f7b49fee48c
shoni <juanigarcia@itba.edu.ar>
2023-06-19 14:40:54 -0300

    Implement Lagrange interpolation

--- include/polynomial.h
+++ include/polynomial.h
@@ -10,6 +10,7 @@ typedef struct Polynomial {
 
 
 Polynomial *init_polynomial(uint8_t degree);
+Polynomial *interpolate(const uint8_t *X, const uint8_t *Y, size_t points, uint8_t mod);
 uint8_t eval(Polynomial *p, uint8_t x, uint8_t mod);
 void destroy_polynomial(Polynomial *p);
 
